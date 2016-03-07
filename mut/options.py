f79c050d5ff65e508ebe5445e558621c2c9dd29c
Andrew Aldridge <i80and@foxquill.com>
2016-03-07 14:02:48 -0500

    options: Fix inheritance

--- mut/options.py
+++ mut/options.py
@@ -61,18 +61,78 @@ class OptionState(mut.State):
     def __init__(self, program: str, name: str) -> None:
         self._replacements = {}  # type: Dict[str, str]
 
-        self.program = program
-        self.name = name
-        self.aliases = None  # type: str
-        self.args = None  # type: str
-        self.default = None  # type: str
-        self.description = None  # type: str
-        self.directive = None  # type: str
-        self.optional = False
-        self.type = None  # type: str
+        self._program = program
+        self._name = name
+        self._aliases = None  # type: str
+        self._args = None  # type: str
+        self._default = None  # type: str
+        self._description = None  # type: str
+        self._directive = None  # type: str
+        self._optional = None  # type: bool
+        self._type = None  # type: str
 
-        self.pre = None  # type: str
-        self.post = None  # type: str
+        self._pre = None  # type: str
+        self._post = None  # type: str
+
+    @property
+    def program(self) -> str: return self._program or ''
+
+    @property
+    def name(self) -> str: return self._name or ''
+
+    @property
+    def aliases(self) -> str: return self._aliases or ''
+
+    @aliases.setter
+    def aliases(self, aliases: str) -> None: self._aliases = aliases
+
+    @property
+    def args(self) -> str: return self._args or ''
+
+    @args.setter
+    def args(self, args: str) -> None: self._args = args
+
+    @property
+    def default(self) -> str: return self._default or ''
+
+    @default.setter
+    def default(self, default: str) -> None: self._default = default
+
+    @property
+    def description(self) -> str: return self._description or ''
+
+    @description.setter
+    def description(self, description: str) -> None: self._description = description
+
+    @property
+    def directive(self) -> str: return self._directive or ''
+
+    @directive.setter
+    def directive(self, directive: str) -> None: self._directive = directive
+
+    @property
+    def optional(self) -> bool: return self._optional or False
+
+    @optional.setter
+    def optional(self, optional: bool) -> None: self._optional = optional
+
+    @property
+    def type(self) -> str: return self._type or ''
+
+    @type.setter
+    def type(self, entry_type: str) -> None: self._type = entry_type
+
+    @property
+    def pre(self) -> str: return self._pre or ''
+
+    @pre.setter
+    def pre(self, pre: str) -> None: self._pre = pre
+
+    @property
+    def post(self) -> str: return self._post or ''
+
+    @post.setter
+    def post(self, post: str) -> None: self._post = post
 
     @property
     def replacements(self) -> Dict[str, str]:
@@ -84,9 +144,9 @@ class OptionState(mut.State):
 
     @property
     def keys(self):
-        return ['program', 'name', 'directive', 'type', 'default',
-                'args', 'description', 'aliases', 'optional', 'pre',
-                'post']
+        return ['_program', '_name', '_directive', '_type', '_default',
+                '_args', '_description', '_aliases', '_optional', '_pre',
+                '_post']
 
 
 class Option:
@@ -213,16 +273,16 @@ class Option:
             raise OptionInputError(path, 'program', 'Missing field "name')
 
         option = cls(program, name, path, config)  # type: Option
-        option.state.aliases = mut.withdraw(value, 'aliases', str, default='')
-        option.state.args = mut.withdraw(value, 'args', str, default='')
+        option.state.aliases = mut.withdraw(value, 'aliases', str)
+        option.state.args = mut.withdraw(value, 'args', str)
         option.state.default = mut.withdraw(value, 'default', str)
-        option.state.description = mut.withdraw(value, 'description', str, default='')
+        option.state.description = mut.withdraw(value, 'description', str)
         option.state.directive = mut.withdraw(value, 'directive', str)
-        option.state.optional = mut.withdraw(value, 'optional', bool, default=False)
-        option.state.type = mut.withdraw(value, 'type', str, default='')
+        option.state.optional = mut.withdraw(value, 'optional', bool)
+        option.state.type = mut.withdraw(value, 'type', str)
 
-        option.state.pre = mut.withdraw(value, 'pre', str, default='')
-        option.state.post = mut.withdraw(value, 'post', str, default='')
+        option.state.pre = mut.withdraw(value, 'pre', str)
+        option.state.post = mut.withdraw(value, 'post', str)
 
         replacements = mut.withdraw(value, 'replacement', mut.str_dict)
         if replacements:
