ac6fd913dcb40d41767a52cfdd9224241c83635a
Andrew Aldridge <i80and@foxquill.com>
2016-10-06 17:16:41 -0400

    mut-images: Refactor

--- mut/build_images.py
+++ mut/build_images.py
@@ -12,38 +12,98 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Usage: mut-images [--edition=<edition>]
+"""Usage: mut-images [<root>]
 
 -h --help           - show this
---edition <edition> - specify the project edition to build.
-
 """
 
+import concurrent.futures
 import logging
 import multiprocessing
+import os
+import subprocess
+import tempfile
+from typing import List
 
 import docopt
 
-from typing import Any, Dict
-
 import mut
-import mut.main
-import mut.images
 
 logger = logging.getLogger(__name__)
 
 
-def main():
+def generate_svg(input_path: str, output_path: str) -> None:
+    """Clean up and minify a SVG file."""
+    logger.info('Generating %s', output_path)
+    inkscape = None
+
+    for path in ('/Applications/Inkscape.app/Contents/Resources/bin/inkscape'):
+        if os.path.isfile(path):
+            inkscape = path
+            break
+
+    if not inkscape:
+        inkscape = 'inkscape'
+
+    input_path = os.path.abspath(input_path)
+    output_path = os.path.abspath(output_path)
+    with tempfile.NamedTemporaryFile() as tmp:
+        subprocess.check_call([inkscape,
+                               input_path,
+                               '--vacuum-defs',
+                               '--export-text-to-path',
+                               '--export-plain-svg', tmp.name],
+                              stdout=subprocess.DEVNULL,
+                              stderr=subprocess.DEVNULL)
+
+        subprocess.check_call(['scour',
+                               '--enable-comment-stripping',
+                               '--enable-id-stripping',
+                               '--shorten-ids',
+                               '--no-line-breaks',
+                               '-q',
+                               '-i', tmp.name,
+                               '-o', output_path])
+
+
+def main() -> None:
     options = docopt.docopt(__doc__)
-    edition = str(options['--edition'] or '')
+    root = str(options['<root>'] or '.')
     logging.basicConfig(level=logging.INFO)
 
-    config = mut.RootConfig('.', edition)
-    config.n_workers = multiprocessing.cpu_count()
+    paths = []  # type: List[str]
+    for root, dirs, files in os.walk(root):
+        for filename in files:
+            components = os.path.splitext(filename)
+            if len(components) < 2 or components[1] != '.svg':
+                continue
 
-    collector = mut.main.FileCollector()
-    collector.walk(config.source_path, ['images'])
-    mut.images.run_build_images(config, collector.get_prefixes(['images']))
+            if components[0].endswith('.bakedsvg'):
+                continue
+
+            paths.append(os.path.join(root, filename))
+
+    logger.info('Build Images: %d', len(paths))
+
+    n_workers = multiprocessing.cpu_count()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=n_workers) as pool:
+        futures = []
+
+        for path in paths:
+            bare_path, _ = os.path.splitext(path)
+            output_filename = bare_path + '.bakedsvg.svg'
+            if not mut.compare_mtimes(output_filename, [path]):
+                continue
+
+            futures.append(pool.submit(
+                generate_svg,
+                path,
+                output_filename))
+
+        for f in futures:
+            exception = f.exception()
+            if exception:
+                logger.error(exception)
 
 if __name__ == '__main__':
     main()
--- mut/images.py
+++ mut/images.py
@@ -1,11 +1,6 @@
-import concurrent.futures
 import logging
-import math
 import os
-import shlex
 import shutil
-import subprocess
-import tempfile
 
 from typing import *
 import rstcloth.rstcloth
@@ -50,7 +45,7 @@ class Image:
         self.name = name
         self.config_path = config_path
         self.path = config.root_config.get_root_path('/source/images/{}.svg'.format(name))
-        self.rendered_path = config.root_config.get_root_path('/source/images/{}-output.svg'.format(name))
+        self.rendered_path = config.root_config.get_root_path('/source/images/{}.bakedsvg.svg'.format(name))
         self.config = config
 
         self.alt = ''
@@ -103,39 +98,6 @@ class Image:
 
             cloth.newline()
 
-    def generate_svg(self, output_path: str):
-        """Clean up and minify this SVG file."""
-        logger.info('Generating %s', output_path)
-        inkscape = None
-
-        for path in ('/usr/bin/inkscape', '/usr/local/bin/inkscape',
-                     '/Applications/Inkscape.app/Contents/Resources/bin/inkscape'):
-            if os.path.exists(path):
-                inkscape = path
-                break
-
-        if not inkscape:
-            logger.error("dependency INKSCAPE not installed. not building images.")
-            return
-
-        cmd = '{cmd} {source} --vacuum-defs --export-text-to-path --export-plain-svg {target}'
-        with tempfile.NamedTemporaryFile() as tmp:
-            cmd = cmd.format(cmd=inkscape, target=tmp.name, source=self.path)
-            r = subprocess.call(shlex.split(cmd), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-
-            if r != 0:
-                logger.warning('error generating image: ' + output_path)
-                logger.error(cmd)
-
-            subprocess.check_call(['scour',
-                                   '--enable-comment-stripping',
-                                   '--enable-id-stripping',
-                                   '--shorten-ids',
-                                   '--no-line-breaks',
-                                   '-q',
-                                   '-i', tmp.name,
-                                   '-o', output_path])
-
     @property
     def output_path(self) -> str:
         return os.path.join(self.config.output_path, self.name) + '.rst'
@@ -174,33 +136,8 @@ def run(root_config: mut.RootConfig, paths: List[str]) -> List[mut.MutInputError
     config = ImagesConfig(root_config)
     for path in paths:
         raw_images = mut.load_yaml(path)
-        [Image.load(raw_image, path, config) for raw_image in raw_images]
+        [Image.load(raw_image, path, config) for raw_image in raw_images if raw_image]
 
     config.output()
-
-    return config.root_config.warnings
-
-
-def run_build_images(root_config: mut.RootConfig, paths: List[str]) -> List[mut.MutInputError]:
-    logger.info('Build Images: %d', len(paths))
-    config = ImagesConfig(root_config)
-    for path in paths:
-        raw_images = mut.load_yaml(path)
-        [Image.load(raw_image, path, config) for raw_image in raw_images]
-
-    with concurrent.futures.ProcessPoolExecutor(max_workers=root_config.n_workers) as pool:
-        futures = []
-
-        for image in config.images:
-            output_filename = os.path.join(os.path.dirname(image.path),
-                                           image.name + '-output.svg')
-            if not mut.compare_mtimes(output_filename, [image.path] + paths):
-                continue
-
-            futures.append(pool.submit(
-                image.generate_svg,
-                output_filename))
-
-        [f.result() for f in futures]
 
     return config.root_config.warnings
