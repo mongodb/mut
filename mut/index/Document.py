0ee0bc6e151664c5f1dbccd39c29b346d0b91540
Andrew Aldridge <i80and@foxquill.com>
2017-08-18 16:11:04 -0400

    Support indexing tutorials; add more type definitions

--- mut/index/Document.py
+++ mut/index/Document.py
@@ -3,7 +3,7 @@ import urllib.parse
 from html5_parser import html_parser
 from lxml import etree
 
-from typing import Any, Callable, Dict, List, Optional
+from typing import Any, Callable, Dict, List, Optional, IO
 
 
 def node_to_text(node) -> str:
@@ -32,16 +32,16 @@ def is_element_of_type(candidate, element_type):
 
 class Document:
     '''Return indexing data from an html document.'''
-    def __init__(self, base_url: str, root_dir: str, path) -> None:
+    def __init__(self, base_url: str, root_dir: str, fh: IO) -> None:
         # Paths
         self._base_url = base_url
         self._root_dir = root_dir
 
         # Document Trees
-        self.html = self.parse_html(path)
+        self.html = self.parse_html(fh)
 
         # Properties
-        self.slug = self.get_url_slug(path)
+        self.slug = self.get_url_slug(fh)
         self.title = self.get_page_title()
         self.headings = self.get_page_headings()
         self.text = self.get_page_text()
@@ -49,26 +49,34 @@ class Document:
         self.tags = self.get_page_tags()
         self.links = self.get_page_links()
 
-    def parse_html(self, path):
+    def parse_html(self, fh: IO):
         '''Return head and content elements of the document.'''
-        capsule = html_parser.parse(path.read(), maybe_xhtml=True)
+        capsule = html_parser.parse(fh.read(), maybe_xhtml=True)
         doc = etree.adopt_external_document(capsule).getroot()
 
         result = {}
         result['head'] = doc.cssselect('head')[0]
-        result['main_content'] = doc.cssselect('.main-column .section')[0]
+
+        for candidate in ('.main-column section', '.main__content'):
+            elements = doc.cssselect(candidate)
+            if elements:
+                result['main_content'] = elements[0]
+                break
+
+        if 'main_content' not in result:
+            raise ValueError('No main content element found')
+
         return result
 
-    def get_url_slug(self, path) -> str:
+    def get_url_slug(self, fh: IO) -> str:
         '''Return the slug after the base url.'''
-        url_slug = str(path.name)[len(self._root_dir):]
+        url_slug = str(fh.name)[len(self._root_dir):]
         return url_slug
 
     @return_text_from_node
     def get_page_title(self):
         '''Return the title of the page.'''
-        page_title = self.html['head'].cssselect('title')[0]
-        return page_title
+        return self.html['head'].cssselect('title')[0]
 
     def get_page_headings(self):
         '''Return all headings (<h1>, <h2>, <h3>).'''
