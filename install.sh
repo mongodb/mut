1fc0ab2e60dbe5779873988c3f8ebd86f3bc0dd0
Andrew Aldridge <i80and@foxquill.com>
2017-08-21 11:53:42 -0400

    install.sh: Force PATH to prefer /usr/local/bin and /usr/bin

--- install.sh
+++ install.sh
@@ -141,7 +141,10 @@ create_venv() {
         cd dev
 
         git clone --depth=1 https://github.com/mongodb/mut.git
-        ( cd mut && pip install -r requirements.txt . )
+        (   cd mut
+            export PATH="/usr/local/bin:/usr/bin:$PATH"
+            pip install -r requirements.txt .
+        )
     )
 
     install_helper mut-build
