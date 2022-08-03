fd365490028f5f543246fa87499167b7f5350a14
Allison Reinheimer Moore <allison@mongodb.com>
2022-08-03 11:05:47 -0400

    cleanup with Heli's feedback

--- mut/index/SnootyManifest.py
+++ mut/index/SnootyManifest.py
@@ -3,30 +3,44 @@ from bson import decode_all
 from jsonpath_ng.ext import parse
 from os import walk
 from os.path import join
+from pathlib import Path
 from json import dumps
 from datetime import datetime
 from concurrent.futures import ProcessPoolExecutor
+import logging
 
-from typing import Any, Dict, Optional, List
+from typing import Any, Dict, Optional, List, Tuple, TypedDict
 
-class Document:
+logger=logging.getLogger(__name__)
+
+class ManifestEntry(TypedDict):
+    slug: str
+    title: str
+    headings: Tuple[Optional[str], Optional[List[str]]]
+    paragraphs: str
+    code: dict
+    preview: str
+    tags: list[str]
+
+class Document():
     '''Return indexing data from a page's AST for search purposes.'''
+  
     def __init__(self, data) -> None:
 
         self.tree = data[0]
 
-        self.robots, self.keywords, self.description = self.findMetadata()
+        self.robots, self.keywords, self.description = self.find_metadata()
 
-        self.paragraphs = self.findParagraphs()
-        self.code = self.findCode()
-        self.title, self.headings = self.findHeadings()
-        self.slug = self.deriveSlug()
-        self.preview = self.derivePreview()
+        self.paragraphs = self.find_paragraphs()
+        self.code = self.find_code()
+        self.title, self.headings = self.find_headings()
+        self.slug = self.derive_slug()
+        self.preview = self.derive_preview()
 
         self.noindex, self.reasons = self.get_noindex()
 
-    def findParagraphs(self) -> str:
-        logger("\tFinding paragraphs")
+    def find_paragraphs(self) -> str:
+        logger.debug("Finding paragraphs")
         # NB: paragraphs include "paragraph" nodes within tables
         jsonpath_expr = parse('$..children[?(@.type==\'paragraph\')]..value')
         results = jsonpath_expr.find(self.tree)
@@ -38,8 +52,8 @@ class Document:
             
         return ' '.join(str_list)
 
-    def findCode(self):
-        logger("\tFinding code")
+    def find_code(self):
+        logger.debug("Finding code")
         jsonpath_expr = parse('$..children[?(@.type==\'code\')]')
         results = jsonpath_expr.find(self.tree)
         code_contents = []
@@ -49,8 +63,8 @@ class Document:
         
         return code_contents
 
-    def findHeadings(self):
-        logger("\tFinding headings and title")
+    def find_headings(self) -> Tuple[Optional[str], Optional[List[str]]]:
+        logger.debug("Finding headings and title")
         # Get all headings nodes
         jsonpath_expr = parse('$..children[?(@.type==\'heading\')].children')
         results = jsonpath_expr.find(self.tree)
@@ -69,13 +83,13 @@ class Document:
         headings.pop(0)
         return title, headings
 
-    def deriveSlug(self):
-        logger("\t Deriving slug")
+    def derive_slug(self):
+        logger.debug("Deriving slug")
         page_id = self.tree["page_id"]
         return page_id
 
-    def derivePreview(self):
-        logger("\tDeriving document search preview")
+    def derive_preview(self) -> Optional[str]:
+        logger.debug("Deriving document search preview")
         # Set preview to the meta description if one is specified.
         if self.description: 
             return self.description
@@ -95,11 +109,11 @@ class Document:
             return None
 
         
-    def findMetadata(self):
-        logger("\tFinding metadata")
-        robots = True
-        keywords = None
-        description = None
+    def find_metadata(self):
+        logger.debug("Finding metadata")
+        robots: str = True
+        keywords: List[str] = None
+        description: str = None
 
         jsonpath_expr = parse('$..children[?(@.name==\'meta\')]..options')
         results = jsonpath_expr.find(self.tree)
@@ -117,11 +131,11 @@ class Document:
     def get_noindex(self) -> bool:
         # TODO: determine what the index / noindex rules should be
         # with Product (DOP-)
-        logger("\tDetermining indexability")
+        logger.debug("Determining indexability")
         noindex = False
         reasons: List[str] = []
         # If :robots: None in metadata, do not index
-        if self.robots == False:
+        if not self.robots:
             noindex = True
             reasons.append("robots=None in meta directive")
 
@@ -130,14 +144,14 @@ class Document:
             noindex = True
             reasons.append("This page has 0 headings, not even the H1")
 
-        logger("\tnoindex: {}".format(noindex))
+        logger.debug("noindex: {}".format(noindex))
         return noindex, reasons
 
-    def export(self) -> Optional[Dict[str, Any]]:
+    def export(self) -> Optional[ManifestEntry]:
         '''Generate the manifest dictionary entry from the AST source.'''
 
         if self.noindex:
-            logger("\tRefusing to index {} because: {}".format(self.slug, ' '.join(self.reasons)))
+            logger.info("Refusing to index {} because: {}".format(self.slug, ' '.join(self.reasons)))
             return
 
         document = {
@@ -161,7 +175,7 @@ class Manifest:
 
     def add_document(self, document: Dict[str, Any]) -> None:
         '''Add a document to the manifest'''
-        if not document == None:
+        if document:
             self.documents.append(document)
     
     def export(self) -> str:
@@ -173,43 +187,29 @@ class Manifest:
         }
         return dumps(manifest, indent=4)
 
-def process_snooty_manifest_bson(path: str) -> Any:
+def process_snooty_manifest_bson(path: Path) -> Optional[ManifestEntry]:
     '''Generates manifest info for a BSON document.'''
-    with open(path, 'rb') as f:
-        logger(path)
-        data = decode_all(f.read())
-        document = Document(data).export()
+    data = decode_all(path.read_bytes())
+    document = Document(data).export()
     return document
 
 def generate_manifest(ast_source: str, url: str, includeInGlobalSearch: bool) -> str:
     '''Process BSON files and compile a manifest.'''
     manifest = Manifest(url, includeInGlobalSearch)
-    print("Building manifests for {} documents".format(len(ast_source)))
-
     with ProcessPoolExecutor() as executor:
         for bson_doc in executor.map(process_snooty_manifest_bson, ast_source):
             manifest.add_document(bson_doc)
         return manifest
-
+           
 def get_ast_list(walk_dir: str) -> List[str]:
     '''Get full list of BSON paths that need to be processed,
        get rid of files that don't need to be indexed, but exist as AST.'''
     ast_source_paths: List[str] = []
 
     for root, dirs, files in walk(walk_dir):
-        if 'images' in dirs:
-            dirs.remove('images')
-        if 'includes' in dirs:
-            dirs.remove('includes')
-        if 'sharedinclude' in dirs:
-            dirs.remove('sharedinclude')
-        if '.DS_Store' in files:
-            files.remove('.DS_Store')
+        for forbidden_name in set("images", "includes", "sharedinclude", ".DS_STORE").intersection(dirs):
+            dirs.remove(forbidden_name)
         for filename in files:
             ast_source_paths.append(join(root, filename))
     return ast_source_paths
 
-def logger(message: str) -> None:
-    return
-    print(f"{datetime.now()} {message}")
-    return
